d443ecc0bb7d95c87384a0d8955d0efe2b21b276
Richard Whitehill <whiteric@jlab.org>
2022-10-14 17:01:24 -0500

    update Midterm

--- Midterm/prob7_LU.py
+++ Midterm/prob7_LU.py
@@ -23,55 +23,56 @@ def solve_x(A,b):
 
     L,U = LU_factorize(A)
    
-    print('L\n',L)
-    print('U\n',U) 
-    print(np.matmul(L,U)-A)
-
     y = np.zeros(n)  
     y[0] = b[0]/L[0,0]
     for i in range(1,n):
         temp = np.array([y[k]*L[i,k] for k in range(i)])
         y[i] = (b[i] - np.sum(temp))/L[i,i]
 
-    print(np.matmul(L,y)-b)
-    
     x = np.zeros(n)
     x[-1] = y[-1]/U[-1,-1]
     for i in range(n-2,-1,-1):
         temp = np.array([x[k]*U[i,k] for k in range(i+1,n)])
         x[i] = (y[i] - np.sum(temp))/U[i,i]
 
-    print(np.matmul(U,x)-y)
 
     return x
 
 if __name__ == '__main__':
-    
-    n = 5
+
+    n = 500
+    bL = 1
+    bU = 1
     A = np.zeros((n,n))
     for i in range(n):
-        if i != 0:
+        if i > bL-1:
             A[i,i-1] = -1
         A[i,i] = 4
-#        if i < n-2:
-#            A[i,i+2] = -1
-        if i > 1:
-            A[i,i-2] = -1
-        if i != n-1:
+        if i != n-bU:
             A[i,i+1] = -1
+    A_inv = np.linalg.inv(A)
 
-    print(A)
-    
-    b = np.ones(n)
-#    b = np.array([i for i in range(1,n+1)])
-
+    b1 = np.ones(n)
     start = time.time()
-    L,U = LU_factorize(A)
-    x   = solve_x(A,b)
+    x1   = solve_x(A,b1)
     end = time.time()
-    
-    print(end - start)
+    x1_actual = np.matmul(A_inv,b1)
+    print('calculation 1 time:',end - start)
+    print('x from factorization:\n',x1)
+    print('x from inversion:\n',x1_actual)
+    print('difference magnitude:\n',np.linalg.norm(x1_actual-x1,2))
 
+    print()
+   
+    b2 = np.array([i for i in range(1,n+1)])
+    start = time.time()
+    x2   = solve_x(A,b2)
+    end = time.time()
+    x2_actual = np.matmul(A_inv,b2)
+    print('calculation 2 time:',end - start)
+    print('x from factorization:\n',x2)
+    print('x from inversion:\n',x2_actual)
+    print('difference magnitude:\n',np.linalg.norm(x2_actual-x2,2))
 
 
 
--- Midterm/prob7_band.py
+++ Midterm/prob7_band.py
@@ -4,27 +4,39 @@ import numpy as np
 import time
 
 def LU_factorize(A,bL,bU):
+    '''
+    Note that in the literature: bL, bU are defined as the "effective widths" of the columns
+    and rows. Here I defined them to be one less than this (i.e. if the matrix is diagonal bL=
+    bU=0, if the matrix is tridiagonal bL=bU=1)
+    ''' 
     n = np.shape(A)[0]
 
     L = np.zeros(np.shape(A)) 
     U = np.copy(A)
 
+    for i in range(n):
+        if A[i,i] == 0: 
+            print("Cannot factorize band matrix: zero entry on the diagonal")
+            return None
+
     for j in range(n):
         L[j,j] = 1
         for i in range(j+1,min(j+1+bL+1,n)):
             L[i,j] = U[i,j]/U[j,j]
-        for l in range(j+1,min(j+1+bU+1,n)):
-            for m in range(j,n):
+        for l in range(j+1,min(j+1+bL+1,n)):
+            for m in range(j,min(j+bU+1,n)):
                 U[l,m] = U[l,m] - U[j,m]*L[l,j]
     return L,U
 
 def solve_x(A,bL,bU,b): 
     n = np.shape(A)[0]
+    
+    for i in range(n):
+        if A[i,i]  == 0: 
+            print("Cannot factorize band matrix: zero entry on the diagonal")
+            return None
 
     L,U = LU_factorize(A,bL,bU)
-    print('L\n',L)
-    print('U\n',U)
-    print(np.matmul(L,U)-A)
 
     y = np.zeros(n)  
     y[0] = b[0]/L[0,0]
@@ -32,42 +44,50 @@ def solve_x(A,bL,bU,b):
         temp = np.array([y[k]*L[i,k] for k in range(max(0,i-bL),i)])
         y[i] = (b[i] - np.sum(temp))/L[i,i]
 
-    print(np.matmul(L,y)-b)
-    
     x = np.zeros(n)
     x[-1] = y[-1]/U[-1,-1]
     for i in range(n-2,-1,-1):
         temp = np.array([x[k]*U[i,k] for k in range(i+1,min(n,i+1+bU))])
         x[i] = (y[i] - np.sum(temp))/U[i,i]
 
-    print(np.matmul(A,x)-b)
-
     return x
 
 if __name__ == '__main__':
 
-    n = 5
+    n = 500
+    bL = 1
+    bU = 1
     A = np.zeros((n,n))
     for i in range(n):
-        if i != 0:
+        if i > bL-1:
             A[i,i-1] = -1
         A[i,i] = 4
-#        if i < n-2:
-#            A[i,i+2] = -1
-#        if i > 1:
-#            A[i,i-2] = -1
-        if i != n-1:
+        if i != n-bU:
             A[i,i+1] = -1
-    print(A)
 
-    b = np.ones(n)
-#    b = np.array([i for i in range(1,n+1)])
+    A_inv = np.linalg.inv(A)
 
-#    start = time.time()
-    x   = solve_x(A,n,n,b)
-#    end = time.time()
+    b1 = np.ones(n)
+    start = time.time()
+    x1   = solve_x(A,bL,bU,b1)
+    end = time.time()
+    x1_actual = np.matmul(A_inv,b1)
+    print('calculation 1 time:',end - start)
+    print('x from factorization:\n',x1)
+    print('x from inversion:\n',x1_actual)
+    print('difference magnitude:\n',np.linalg.norm(x1_actual-x1,2))
 
-#    print(end - start)
+    print()
+   
+    b2 = np.array([i for i in range(1,n+1)])
+    start = time.time()
+    x2   = solve_x(A,bL,bU,b2)
+    end = time.time()
+    x2_actual = np.matmul(A_inv,b2)
+    print('calculation 2 time:',end - start)
+    print('x from factorization:\n',x2)
+    print('x from inversion:\n',x2_actual)
+    print('difference magnitude:\n',np.linalg.norm(x2_actual-x2,2))
 
 
 
